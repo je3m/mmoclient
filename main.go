8d65060028b445e06ac8cead932a6ba8e2c47622
jeem <jeem@j33m.net>
2024-11-30 08:29:17 -0500

    farmin lvl10 mats

---
+++ main.go
@@ -0,0 +1,513 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type MoveRequest struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type CharacterState struct {
+	Name                 string    `json:"name"`
+	Account              string    `json:"account"`
+	Skin                 string    `json:"skin"`
+	Level                int       `json:"level"`
+	Xp                   int       `json:"xp"`
+	MaxXp                int       `json:"max_xp"`
+	Gold                 int       `json:"gold"`
+	Speed                int       `json:"speed"`
+	MiningLevel          int       `json:"mining_level"`
+	MiningXp             int       `json:"mining_xp"`
+	MiningMaxXp          int       `json:"mining_max_xp"`
+	WoodcuttingLevel     int       `json:"woodcutting_level"`
+	WoodcuttingXp        int       `json:"woodcutting_xp"`
+	WoodcuttingMaxXp     int       `json:"woodcutting_max_xp"`
+	FishingLevel         int       `json:"fishing_level"`
+	FishingXp            int       `json:"fishing_xp"`
+	FishingMaxXp         int       `json:"fishing_max_xp"`
+	WeaponcraftingLevel  int       `json:"weaponcrafting_level"`
+	WeaponcraftingXp     int       `json:"weaponcrafting_xp"`
+	WeaponcraftingMaxXp  int       `json:"weaponcrafting_max_xp"`
+	GearcraftingLevel    int       `json:"gearcrafting_level"`
+	GearcraftingXp       int       `json:"gearcrafting_xp"`
+	GearcraftingMaxXp    int       `json:"gearcrafting_max_xp"`
+	JewelrycraftingLevel int       `json:"jewelrycrafting_level"`
+	JewelrycraftingXp    int       `json:"jewelrycrafting_xp"`
+	JewelrycraftingMaxXp int       `json:"jewelrycrafting_max_xp"`
+	CookingLevel         int       `json:"cooking_level"`
+	CookingXp            int       `json:"cooking_xp"`
+	CookingMaxXp         int       `json:"cooking_max_xp"`
+	AlchemyLevel         int       `json:"alchemy_level"`
+	AlchemyXp            int       `json:"alchemy_xp"`
+	AlchemyMaxXp         int       `json:"alchemy_max_xp"`
+	Hp                   int       `json:"hp"`
+	MaxHp                int       `json:"max_hp"`
+	Haste                int       `json:"haste"`
+	CriticalStrike       int       `json:"critical_strike"`
+	Stamina              int       `json:"stamina"`
+	AttackFire           int       `json:"attack_fire"`
+	AttackEarth          int       `json:"attack_earth"`
+	AttackWater          int       `json:"attack_water"`
+	AttackAir            int       `json:"attack_air"`
+	DmgFire              int       `json:"dmg_fire"`
+	DmgEarth             int       `json:"dmg_earth"`
+	DmgWater             int       `json:"dmg_water"`
+	DmgAir               int       `json:"dmg_air"`
+	ResFire              int       `json:"res_fire"`
+	ResEarth             int       `json:"res_earth"`
+	ResWater             int       `json:"res_water"`
+	ResAir               int       `json:"res_air"`
+	X                    int       `json:"x"`
+	Y                    int       `json:"y"`
+	Cooldown             int       `json:"cooldown"`
+	CooldownExpiration   time.Time `json:"cooldown_expiration"`
+	WeaponSlot           string    `json:"weapon_slot"`
+	ShieldSlot           string    `json:"shield_slot"`
+	HelmetSlot           string    `json:"helmet_slot"`
+	BodyArmorSlot        string    `json:"body_armor_slot"`
+	LegArmorSlot         string    `json:"leg_armor_slot"`
+	BootsSlot            string    `json:"boots_slot"`
+	Ring1Slot            string    `json:"ring1_slot"`
+	Ring2Slot            string    `json:"ring2_slot"`
+	AmuletSlot           string    `json:"amulet_slot"`
+	Artifact1Slot        string    `json:"artifact1_slot"`
+	Artifact2Slot        string    `json:"artifact2_slot"`
+	Artifact3Slot        string    `json:"artifact3_slot"`
+	Utility1Slot         string    `json:"utility1_slot"`
+	Utility1SlotQuantity int       `json:"utility1_slot_quantity"`
+	Utility2Slot         string    `json:"utility2_slot"`
+	Utility2SlotQuantity int       `json:"utility2_slot_quantity"`
+	Task                 string    `json:"task"`
+	TaskType             string    `json:"task_type"`
+	TaskProgress         int       `json:"task_progress"`
+	TaskTotal            int       `json:"task_total"`
+	InventoryMaxItems    int       `json:"inventory_max_items"`
+	Inventory            []struct {
+		Slot     int    `json:"slot"`
+		Code     string `json:"code"`
+		Quantity int    `json:"quantity"`
+	} `json:"inventory"`
+}
+
+type ActionResponse struct {
+	Data struct {
+		Cooldown struct {
+			TotalSeconds     int       `json:"total_seconds"`
+			RemainingSeconds int       `json:"remaining_seconds"`
+			StartedAt        time.Time `json:"started_at"`
+			Expiration       time.Time `json:"expiration"`
+			Reason           string    `json:"reason"`
+		} `json:"cooldown"`
+		Details struct {
+			Xp    int `json:"xp"`
+			Items []struct {
+				Code     string `json:"code"`
+				Quantity int    `json:"quantity"`
+			} `json:"items"`
+		} `json:"details"`
+		Character CharacterState `json:"character"`
+	} `json:"data"`
+}
+
+// I think go makes you do all this shit manually :c
+func updateInventory(state *CharacterState, resp *ActionResponse) {
+	state.Inventory = nil
+	for _, slot := range resp.Data.Character.Inventory {
+		state.Inventory = append(state.Inventory, slot)
+	}
+	state.InventoryMaxItems = resp.Data.Character.InventoryMaxItems
+
+}
+
+// perform given action and block until cooldown is up
+func performActionAndWait(state *CharacterState, actionName string, actionData []byte) (*ActionResponse, error) {
+	response := new(ActionResponse)
+
+	// Define the endpoint and token
+	apiURL := "https://api.artifactsmmo.com/my/" + state.Name + "/action/" + actionName
+	token := ""
+
+	// Create the HTTP request
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(actionData))
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		return nil, err
+	}
+
+	// Set headers
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error making request: %v\n", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read and display the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %v\n", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Request failed with status: %s\n", ArtifactsResponseCode(resp.StatusCode))
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		fmt.Printf("Error parsing response: %v\n", err)
+		return nil, err
+	}
+
+	updateInventory(state, response)
+
+	cooldown := response.Data.Cooldown.RemainingSeconds
+	fmt.Printf("Waiting %v seconds to finish action %s\n", cooldown, actionName)
+	time.Sleep(time.Duration(cooldown) * time.Second)
+
+	return response, err
+}
+
+func move(state *CharacterState, x int, y int) error {
+	moveRequest := MoveRequest{
+		X: x,
+		Y: y,
+	}
+
+	// Marshal the request body to JSON
+	requestBody, err := json.Marshal(moveRequest)
+	if err != nil {
+		fmt.Printf("Error marshalling request body: %v\n", err)
+		return err
+	}
+
+	_, err = performActionAndWait(state, "move", requestBody)
+	return err
+}
+
+func fight(state *CharacterState) {
+	performActionAndWait(state, "fight", []byte{})
+}
+
+func rest(state *CharacterState) {
+	performActionAndWait(state, "rest", []byte{})
+}
+
+func gathering(state *CharacterState) {
+	performActionAndWait(state, "gathering", []byte{})
+}
+
+func getItemInventoryQty(response *ActionResponse, itemName string) int {
+	inv := response.Data.Character.Inventory
+	for _, item := range inv {
+		if item.Code == itemName {
+			return item.Quantity
+		}
+	}
+	return 0
+}
+
+// Perform gathering action until inventory contains at least <quantity> of item
+func gatherUntil(state *CharacterState, item string, quantity int) error {
+	numberRemaining := 1
+
+	for numberRemaining > 0 {
+		if getInventoryFull(state) {
+			fmt.Printf("Inventory full. returning early\n")
+			break
+		}
+
+		resp, err := performActionAndWait(state, "gathering", []byte{})
+		if err != nil {
+			fmt.Printf("Error making request: %v\n", err)
+			return err
+		}
+		numberHas := getItemInventoryQty(resp, item)
+		numberRemaining = quantity - numberHas
+
+		fmt.Printf(
+			"Have: %v\n"+
+				"Need: %v\n"+
+				"Remaining: %v\n", numberHas, quantity, numberRemaining)
+
+	}
+	return nil
+}
+
+func unequip(state *CharacterState, slot string) {
+	type UnequipRequest struct {
+		Slot string `json:"slot"`
+	}
+
+	jsonData, err := json.Marshal(UnequipRequest{slot})
+	if err != nil {
+		fmt.Printf("Error marshalling request body: %v\n", err)
+		os.Exit(1)
+	}
+
+	performActionAndWait(state, "unequip", jsonData)
+}
+
+func moveWeaponCraftStation(state *CharacterState) {
+	move(state, 2, 1)
+}
+func craftItem(state *CharacterState, code string) {
+	type CraftItemRequest struct {
+		Code string `json:"code"`
+	}
+	jsonData, err := json.Marshal(CraftItemRequest{code})
+	if err != nil {
+		fmt.Printf("Error marshalling request body: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = performActionAndWait(state, "crafting", jsonData)
+	if err != nil {
+		fmt.Printf("Error making crafting item: %v\n", err)
+	}
+}
+
+func equipItem(state *CharacterState, code string, slot string) error {
+	type EquipItemRequest struct {
+		Code string `json:"code"`
+		Slot string `json:"slot"`
+	}
+	jsonData, err := json.Marshal(EquipItemRequest{code, slot})
+	if err != nil {
+		fmt.Printf("Error marshalling request body: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = performActionAndWait(state, "equip", jsonData)
+	if err != nil {
+		fmt.Printf("Error equiping item: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToAshTreeForest(state *CharacterState) error {
+	err := move(state, -1, 0)
+	if err != nil {
+		fmt.Printf("Failed to move to ash tree forest: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToCopperMine(state *CharacterState) error {
+	err := move(state, 2, 0)
+	if err != nil {
+		fmt.Printf("Failed to move to ash tree forest: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToIronMine(state *CharacterState) error {
+	err := move(state, 1, 7)
+	if err != nil {
+		fmt.Printf("Failed to move to iron mine: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToBank(state *CharacterState) error {
+	err := move(state, 4, 1)
+	if err != nil {
+		fmt.Printf("Failed to move to bank: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToSpruce(state *CharacterState) error {
+	err := move(state, 2, 6)
+	if err != nil {
+		fmt.Printf("Failed to move to spruce: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToSunflower(state *CharacterState) error {
+	err := move(state, 2, 2)
+	if err != nil {
+		fmt.Printf("Failed to move to bank: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToShrimp(state *CharacterState) error {
+	err := move(state, 5, 2)
+	if err != nil {
+		fmt.Printf("Failed to move to shrimp: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func moveToGudgeon(state *CharacterState) error {
+	err := move(state, 4, 2)
+	if err != nil {
+		fmt.Printf("Failed to move to bank: %v\n", err)
+		return err
+	}
+	return nil
+}
+func depositItemAtBank(state *CharacterState, code string, qty int) error {
+	type DepositItemRequest struct {
+		Code     string `json:"code"`
+		Quantity int    `json:"quantity"`
+	}
+	jsonData, err := json.Marshal(DepositItemRequest{code, qty})
+	if err != nil {
+		fmt.Printf("Error marshalling request body: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = performActionAndWait(state, "bank/deposit", jsonData)
+	if err != nil {
+		fmt.Printf("Error depositing item: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func dumpAtBank(state *CharacterState) {
+	moveToBank(state)
+
+	for _, item := range state.Inventory {
+		if item.Quantity > 0 {
+			depositItemAtBank(state, item.Code, item.Quantity)
+		}
+	}
+}
+
+func getInventoryFull(state *CharacterState) bool {
+	inv := state.Inventory
+	count := 0
+	for _, item := range inv {
+		count += item.Quantity
+	}
+
+	return state.InventoryMaxItems <= count
+}
+
+func squidwardLoop(currentCharacter *CharacterState) error {
+	for {
+		dumpAtBank(currentCharacter)
+
+		err := moveToIronMine(currentCharacter)
+		if err != nil {
+			os.Exit(1)
+		}
+
+		err = gatherUntil(currentCharacter, "iron_ore", 100)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func chadLoop(currentCharacter *CharacterState) error {
+	for {
+		dumpAtBank(currentCharacter)
+
+		err := moveToSpruce(currentCharacter)
+		if err != nil {
+			return err
+		}
+		err = gatherUntil(currentCharacter, "spruce_wood", 100)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func lilyLoop(currentCharacter *CharacterState) error {
+	for {
+		dumpAtBank(currentCharacter)
+
+		err := moveToSunflower(currentCharacter)
+		if err != nil {
+			fmt.Printf("Failed to move to sunflower: %v\n", err)
+			return err
+		}
+		err = gatherUntil(currentCharacter, "sunflower", 100)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func timothyLoop(currentCharacter *CharacterState) error {
+	for {
+		dumpAtBank(currentCharacter)
+
+		err := moveToShrimp(currentCharacter)
+		if err != nil {
+			fmt.Printf("Failed to move to shrimp: %v\n", err)
+			return err
+		}
+		err = gatherUntil(currentCharacter, "shrimp", 100)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+	characterMap := make(map[string]CharacterState)
+	characterMap["squidward"] = CharacterState{Name: "squidward"}
+	chadState := CharacterState{Name: "chad"}
+	squidwardState := CharacterState{Name: "squidward"}
+	lilyState := CharacterState{Name: "lily"}
+	timothyState := CharacterState{Name: "timothy"}
+
+	go func() {
+		err := chadLoop(&chadState)
+		if err != nil {
+			fmt.Printf("Failed to chad loop: %v\n", err)
+		}
+	}()
+	go func() {
+		err := squidwardLoop(&squidwardState)
+		if err != nil {
+			fmt.Printf("Failed to squward loop: %v\n", err)
+		}
+	}()
+	go func() {
+		err := lilyLoop(&lilyState)
+		if err != nil {
+			fmt.Printf("Failed to lily loop: %v\n", err)
+		}
+	}()
+
+	go func() {
+		err := timothyLoop(&timothyState)
+		if err != nil {
+			fmt.Printf("Failed to timothy loop: %v\n", err)
+		}
+	}()
+	var wg = sync.WaitGroup{}
+	wg.Add(1)
+	defer wg.Wait()
+
+}
---
+++ response_codes.go
@@ -0,0 +1,180 @@
+package main
+
+type ArtifactsResponseCode int
+
+const (
+	// general
+	CodeInvalidPayload  ArtifactsResponseCode = 422
+	CodeTooManyRequests ArtifactsResponseCode = 429
+	CodeNotFound        ArtifactsResponseCode = 404
+	CodeFatalError      ArtifactsResponseCode = 500
+
+	// account error codes
+	CodeTokenInvalid           ArtifactsResponseCode = 452
+	CodeTokenExpired           ArtifactsResponseCode = 453
+	CodeTokenMissing           ArtifactsResponseCode = 454
+	CodeTokenGenerationFail    ArtifactsResponseCode = 455
+	CodeUsernameAlreadyUsed    ArtifactsResponseCode = 456
+	CodeEmailAlreadyUsed       ArtifactsResponseCode = 457
+	CodeSamePassword           ArtifactsResponseCode = 458
+	CodeCurrentPasswordInvalid ArtifactsResponseCode = 459
+
+	// character error codes
+	CodeCharacterNotEnoughHp            ArtifactsResponseCode = 483
+	CodeCharacterMaximumUtilitesEquiped ArtifactsResponseCode = 484
+	CodeCharacterItemAlreadyEquiped     ArtifactsResponseCode = 485
+	CodeCharacterLocked                 ArtifactsResponseCode = 486
+	CodeCharacterNotThisTask            ArtifactsResponseCode = 474
+	CodeCharacterTooManyItemsTask       ArtifactsResponseCode = 475
+	CodeCharacterNoTask                 ArtifactsResponseCode = 487
+	CodeCharacterTaskNotCompleted       ArtifactsResponseCode = 488
+	CodeCharacterAlreadyTask            ArtifactsResponseCode = 489
+	CodeCharacterAlreadyMap             ArtifactsResponseCode = 490
+	CodeCharacterSlotEquipmentError     ArtifactsResponseCode = 491
+	CodeCharacterGoldInsufficient       ArtifactsResponseCode = 492
+	CodeCharacterNotSkillLevelRequired  ArtifactsResponseCode = 493
+	CodeCharacterNameAlreadyUsed        ArtifactsResponseCode = 494
+	CodeMaxCharactersReached            ArtifactsResponseCode = 495
+	CodeCharacterNotLevelRequired       ArtifactsResponseCode = 496
+	CodeCharacterInventoryFull          ArtifactsResponseCode = 497
+	CodeCharacterNotFound               ArtifactsResponseCode = 498
+	CodeCharacterInCooldown             ArtifactsResponseCode = 499
+
+	// item error codes
+	CodeItemInsufficientQuantity ArtifactsResponseCode = 471
+	CodeItemInvalidEquipment     ArtifactsResponseCode = 472
+	CodeItemRecyclingInvalidItem ArtifactsResponseCode = 473
+	CodeItemInvalidConsumable    ArtifactsResponseCode = 476
+	CodeMissingItem              ArtifactsResponseCode = 478
+
+	// grand exchange error codes
+	CodeGeMaxQuantity           ArtifactsResponseCode = 479
+	CodeGeNotInStock            ArtifactsResponseCode = 480
+	CodeGeNotThePrice           ArtifactsResponseCode = 482
+	CodeGeTransactionInProgress ArtifactsResponseCode = 436
+	CodeGeNoOrders              ArtifactsResponseCode = 431
+	CodeGeMaxOrders             ArtifactsResponseCode = 433
+	CodeGeTooManyItems          ArtifactsResponseCode = 434
+	CodeGeSameAccount           ArtifactsResponseCode = 435
+	CodeGeInvalidItem           ArtifactsResponseCode = 437
+	CodeGeNotYourOrder          ArtifactsResponseCode = 438
+
+	// bank error codes
+	CodeBankInsufficientGold      ArtifactsResponseCode = 460
+	CodeBankTransactionInProgress ArtifactsResponseCode = 461
+	CodeBankFull                  ArtifactsResponseCode = 462
+
+	// maps error codes
+	CodeMapNotFound        ArtifactsResponseCode = 597
+	CodeMapContentNotFound ArtifactsResponseCode = 598
+)
+
+func (me ArtifactsResponseCode) String() string {
+	switch me {
+	case CodeInvalidPayload:
+		return "InvalidPayload"
+	case CodeTooManyRequests:
+		return "TooManyRequests"
+	case CodeNotFound:
+		return "NotFound"
+	case CodeFatalError:
+		return "FatalError"
+	case CodeTokenInvalid:
+		return "TokenInvalid"
+	case CodeTokenExpired:
+		return "TokenExpired"
+	case CodeTokenMissing:
+		return "TokenMissing"
+	case CodeTokenGenerationFail:
+		return "TokenGenerationFail"
+	case CodeUsernameAlreadyUsed:
+		return "UsernameAlreadyUsed"
+	case CodeEmailAlreadyUsed:
+		return "EmailAlreadyUsed"
+	case CodeSamePassword:
+		return "SamePassword"
+	case CodeCurrentPasswordInvalid:
+		return "CurrentPasswordInvalid"
+	case CodeCharacterNotEnoughHp:
+		return "CharacterNotEnoughHp"
+	case CodeCharacterMaximumUtilitesEquiped:
+		return "CharacterMaximumUtilitesEquiped"
+	case CodeCharacterItemAlreadyEquiped:
+		return "CharacterItemAlreadyEquiped"
+	case CodeCharacterLocked:
+		return "CharacterLocked"
+	case CodeCharacterNotThisTask:
+		return "CharacterNotThisTask"
+	case CodeCharacterTooManyItemsTask:
+		return "CharacterTooManyItemsTask"
+	case CodeCharacterNoTask:
+		return "CharacterNoTask"
+	case CodeCharacterTaskNotCompleted:
+		return "CharacterTaskNotCompleted"
+	case CodeCharacterAlreadyTask:
+		return "CharacterAlreadyTask"
+	case CodeCharacterAlreadyMap:
+		return "CharacterAlreadyMap"
+	case CodeCharacterSlotEquipmentError:
+		return "CharacterSlotEquipmentError"
+	case CodeCharacterGoldInsufficient:
+		return "CharacterGoldInsufficient"
+	case CodeCharacterNotSkillLevelRequired:
+		return "CharacterNotSkillLevelRequired"
+	case CodeCharacterNameAlreadyUsed:
+		return "CharacterNameAlreadyUsed"
+	case CodeMaxCharactersReached:
+		return "MaxCharactersReached"
+	case CodeCharacterNotLevelRequired:
+		return "CharacterNotLevelRequired"
+	case CodeCharacterInventoryFull:
+		return "CharacterInventoryFull"
+	case CodeCharacterNotFound:
+		return "CharacterNotFound"
+	case CodeCharacterInCooldown:
+		return "CharacterInCooldown"
+	case CodeItemInsufficientQuantity:
+		return "ItemInsufficientQuantity"
+	case CodeItemInvalidEquipment:
+		return "ItemInvalidEquipment"
+	case CodeItemRecyclingInvalidItem:
+		return "ItemRecyclingInvalidItem"
+	case CodeItemInvalidConsumable:
+		return "ItemInvalidConsumable"
+	case CodeMissingItem:
+		return "MissingItem"
+	case CodeGeMaxQuantity:
+		return "GeMaxQuantity"
+	case CodeGeNotInStock:
+		return "GeNotInStock"
+	case CodeGeNotThePrice:
+		return "GeNotThePrice"
+	case CodeGeTransactionInProgress:
+		return "GeTransactionInProgress"
+	case CodeGeNoOrders:
+		return "GeNoOrders"
+	case CodeGeMaxOrders:
+		return "GeMaxOrders"
+	case CodeGeTooManyItems:
+		return "GeTooManyItems"
+	case CodeGeSameAccount:
+		return "GeSameAccount"
+	case CodeGeInvalidItem:
+		return "GeInvalidItem"
+	case CodeGeNotYourOrder:
+		return "GeNotYourOrder"
+	case CodeBankInsufficientGold:
+		return "BankInsufficientGold"
+	case CodeBankTransactionInProgress:
+		return "BankTransactionInProgress"
+	case CodeBankFull:
+		return "BankFull"
+	case CodeMapNotFound:
+		return "MapNotFound"
+	case CodeMapContentNotFound:
+		return "MapContentNotFound"
+	default:
+		return "BAD_ERROR_CODE"
+
+	}
+}
